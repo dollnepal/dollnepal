65ed8dcaa87231b08306ba8d4913899c65e454b2
dollnepal <154342974+dollnepal@users.noreply.github.com>
2023-12-22 19:31:00 +0545

    files modified

---
+++ dollnepalwebsite/product.js
@@ -0,0 +1,87 @@
+
+// const allHoverImages = document.querySelectorAll('.hover-container div img');
+// const imgContainer = document.querySelector('.img-container');
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     allHoverImages[0].parentElement.classList.add('active');
+// });
+
+// allHoverImages.forEach((image) => {
+//     image.addEventListener('mouseover', () =>{
+//         imgContainer.querySelector('img').src = image.src;
+//         resetActiveImg();
+//         image.parentElement.classList.add('active');
+//     });
+// });
+
+// function resetActiveImg(){
+//     allHoverImages.forEach((img) => {
+//         img.parentElement.classList.remove('active');
+//     });
+// }
+
+
+// var mainimg = document.getElementById('main');
+// var smallimg = document.getElementsByClassName('small-img');
+
+// smallimg[0].onclick = function(){
+//     mainimg.src = smallimg[0].src;
+// }
+// smallimg[1].onclick = function (){
+//     mainimg.src = smallimg[1].src;
+// }
+// smallimg[2].onclick = function (){
+//     mainimg.src = smallimg[2].src;
+// }
+// smallimg[3].onclick = function (){
+//     mainimg.src = smallimg[3].src;
+// }
+// smallimg[4].onclick = function (){
+//     mainimg.src = smallimg[4].src;
+// }
+
+
+// var mainimg = document.getElementById('main');
+// var smallimg = document.getElementsByClassName('small-img');
+
+// function small1(){
+//     document.getElementById('main').src = 'assets/images/3ft/cream3.jpg';
+// }
+// function small2(){
+//     document.getElementById('main').src = 'assets/images/3ft/brown3.JPG';
+// }
+// function small3(){
+//     document.getElementById('main').src = 'assets/images/3ft/pink3.jpg';
+// }
+// function small4(){
+//     document.getElementById('main').src = 'assets/images/3ft/purple3.jpg';
+// }
+// function small5(){
+//     document.getElementById('main').src = 'assets/images/3ft/yellow3.JPG';
+// }
+
+
+
+
+
+
+
+// let ok = document.querySelector("#hover-container");
+// let isShow = true;
+
+// function hideShow(){
+//     if(isShow){
+//         ok.style.display = "none";
+//         isShow = false;
+//     }
+//     else{
+//         ok.style.display = "block";
+//         isShow = true;
+//     }
+
+// }
+
+
+
+
+
